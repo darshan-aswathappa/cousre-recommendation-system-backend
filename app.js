1ec6743523404827f5e8db55fee162b6bbf4c8e2
louyanqing <lou.yan@northeastern.edu>
2024-12-03 16:44:46 -0500

    Fix port error

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const { default: mongoose } = require("mongoose");
 const cors = require("cors");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.LOCAL_PORT || 3000;
 
 app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 
